765903d20a266a586d82e988d0d7d18545afe4b7
heba <hebe10@yahoo.com>
2020-04-20 21:49:27 +0200

    show titles of the posts and skills and work exp on front page

--- front-page.php
+++ front-page.php
@@ -1,24 +1,80 @@
-<?php get_header();?>
-<div class="container">
 
-<?php 
+   <?php get_header(); ?>
+   <section class="skills">
+    <h1>Skills</h1>
 
-if(have_posts()):
-    while(have_posts()): the_post();?>
-    <div class="row">
-<div class="col-md-10">
-    <h2><?php the_title();?></h2>
-</div>
-</div>
-   
+    <div class="container">
     <?php
-endwhile;
-endif;
-?>
-</div>
+            $args = array(
+                "post_type" => ["skills"],
+                "posts_per_page" => 2
+            );
+            $query = new WP_Query($args);
+            if($query->have_posts(  )){
+                while($query->have_posts(  )){ $query->the_post(); ?>
+            <div class="row">
+                <div class="col-md-6">
+                    <h2><?php the_title(); ?></h2>
+                </div>
+            </div>
+            <?php 
+                }
+            }
+
+            ?>
+    </div>
+</section>
 
 
+<section class="work-exp">
+    <h1>Work Experience</h1>
+    <div class="container">
+    <?php
+            $args = array(
+                "post_type" => ["work_experiences"],
+                "posts_per_page" => 2
+            );
+            $query = new WP_Query($args);
+            if($query->have_posts(  )){
+                while($query->have_posts(  )){ $query->the_post(); ?>
+            <div class="row">
+                <div class="col-md-6">
+                    <h2><?php the_title(); ?></h2>
+                </div>
+            </div>
+            <?php 
+                }
+            }
 
+            ?>
+    </div>
+</section>
 
-</div>
-<?php get_footer();?>
+<section class="work-exp">
+    <h1>Blog</h1>
+    <div class="container">
+    <?php
+            $args = array(
+                "post_type" => ["post"],
+                "posts_per_page" => 2
+            );
+            $query = new WP_Query($args);
+            if($query->have_posts(  )){
+                while($query->have_posts(  )){ $query->the_post(); ?>
+            <div class="row">
+                <div class="col-md-6">
+                    <h2><?php the_title(); ?></h2>
+                </div>
+            </div>
+            <?php 
+                }
+            }
+
+            ?>
+    </div>
+</section>
+    
+                
+                
+   
+   <?php get_footer(); ?>
