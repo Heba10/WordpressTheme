0c875b4870c916f5ea0393c42a51a181d29bf104
heba <hebe10@yahoo.com>
2020-04-21 14:31:46 +0200

    add about page style and content

--- page-about.php
+++ page-about.php
@@ -1,3 +1,59 @@
 <?php get_header();?>
-<h1>about.php</h1>
+<main id="about" >
+    <h1 class="lg-heading" >
+      About
+      <span class="text-secondary">Me</span>
+    </h1>
+
+     <h2 class="sm-heading">
+      Let me tell you a few things...
+    </h2>
+    </div>
+		<div class="container">
+		<div class="jumbotron">
+			<img src="img/4.jpg" width="120px"  class="img-responsive pull-right img-circle">
+		
+        <h3 class="text-secondary">Hello, </h3>
+        <p class="text-p">
+I am a Heba, I am very excited for every field in developing the game, and I enjoy making websites ♥. I have a lot of enthusiasm when it comes to Marketing ,Design and any type of Art  ,I love learning new techniques, learning new things and facing any challenge! I  graduated in June 2019 from Alexandria University Faculty of Science, which studies computer science.</p>
+      </div>
+		</div>
+
+<!-- 
+               /////////////////////////////// -->
+		<div class="row">
+					<div class="col-lg-4 col-xs-6">
+
+						<div class="job job-1">
+        <h3>Art Director</h3>
+        <h6>🎨Freelancer Graphic Designer 🎨</h6>
+        <p>
+        Website Design | LOGO Design | Brand Design |</p>
+      </div>
+       		<p><a href="#art" class="btn btn-primary">See More>></a></p>
+					</div>
+
+					<div class="col-lg-4 col-xs-6">
+						<div class="job job-2">
+        <h3>Webworks</h3>
+        <h6>Full Stack Developer</h6>
+        <h3>UI/UX Designer</h3>
+        <h6>Front End Developer</h6>
+        <p>
+          I'm a creative web designer. I do live Web design, Mobile App design,
+          UI&UX design and other.</p>
+      </div>
+						<p><a href="#web" class="btn btn-warning">See More>></a></p>
+					</div>
+					<div class="col-lg-4 col-xs-6">
+						   <div class="job job-3">
+        
+        <h3>Game Designer</h3>
+        <p>Make the creative game 2d ,3d by using unity engine  </p>
+      </div>
+      </div>
+						<p><a href="#game" class="btn btn-danger">See More>></a></p>
+					</div>
+</div>
+</div></div></main>
 <?php get_footer();?>
